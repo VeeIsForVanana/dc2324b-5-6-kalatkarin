3668f09f0f1e7e6ea63b0453c4edeb328cffedec
kalatkarin <carlynecconsebido@gmail.com>
2024-03-25 16:07:58 +0800

    feat(Container.svelte, WonkierButton.svelte): added code

--- src/routes/Container.svelte
+++ src/routes/Container.svelte
@@ -1,1 +1,3 @@
 <div data-testid="container"></div>
+
+<slot />
--- src/routes/WonkierButton.svelte
+++ src/routes/WonkierButton.svelte
@@ -1,21 +1,60 @@
 <script>
     import Container from "./Container.svelte";
+	
+    export let value = 0;
+    let random = 0;
+    let curr = "FizzBuzz";
+
+    function handleClick() {
+	    random = Math.floor(Math.random() * 10);
+	    value += random;
+
+        if (value % 3 == 0 && value % 5 == 0){
+            curr = "FizzBuzz";
+        }
+        else if (value % 3 == 0){
+            curr = "Fizz";
+        }
+        else if (value % 5 == 0){
+            curr = "Buzz";
+        }
+        else {
+            curr = "Default";
+        }
+    }
+
+    let isDisabled = false;
+
+    function toggleLock() {
+        isDisabled = !isDisabled;
+    }
+    
 </script>
 
 <Container>
-    <div data-testid="text-input" contenteditable />
+    <div data-testid="text-input" contenteditable bind:innerHTML={curr} />
 
-    <button data-testid="lock-button"> </button>
+    <button data-testid="lock-button" on:click={toggleLock}> 
+        Lock
+    </button>
 
-    <button data-testid="wonkier-button"> </button>
+    <button data-testid="wonkier-button" on:click={handleClick} class="{isDisabled ? 'locked' : curr}" disabled={isDisabled}> 
+        {#if value % 3 == 0 && value % 5 == 0}
+            {'FizzBuzz ' + value}
+        {:else if value % 3 == 0}
+            {'Fizz ' + value}
+        {:else if value % 5 == 0}
+            {'Buzz ' + value}
+        {:else}
+            {value}
+        {/if}
+    </button>
 
-    <!--
-        <p>FizzBuzz!</p> 
+    {#if curr == "FizzBuzz"}
+        <p> FizzBuzz! </p> 
+    {/if}
 
-        should be rendered when the text inside the wonkier-button starts with FizzBuzz
-    -->
 </Container>
-
 
 <style>
     [contenteditable] {
